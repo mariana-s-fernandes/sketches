4c5c1171bb7b876416c3deecada0413c8757c6c1
Mariana Fernandes <marianasf13@hotmail.com>
2022-04-23 20:25:36 +0100

    Comments added and linting

--- CMYK_Halftone/cmyk_halftone.py
+++ CMYK_Halftone/cmyk_halftone.py
@@ -8,19 +8,25 @@ import glob
 
 from vpype.utils import PAGE_SIZES
 
+# Angles for each layer of the image
 CMYK_ANGLES = [15, 75, 0, 45]
-CMYK_COLORS = [(0,255,255), (255,0,255), (255,255,0)]
-ext = ['jpg', 'png']
+CMYK_COLORS = [(0, 255, 255), (255, 0, 255), (255, 255, 0)]
+# Get all images in directory
+ext = ["jpg", "png"]
 images = []
-[images.extend(glob.glob('*.' + e)) for e in ext]
-IMG_PATH = 'mountains.jpg'
+[images.extend(glob.glob("*." + e)) for e in ext]
+IMG_PATH = "mountains.jpg"
+
 
 def cmyk_radius(value, maxRadius):
-    r = (255-value) /255 * maxRadius
-    return  r
+    """Calculates circle radius depending on color intensity"""
+    r = (255 - value) / 255 * maxRadius
+    return r
+
 
 def map_range(value, start1, stop1, start2, stop2):
-   return (value - start1) / (stop1 - start1) * (stop2 - start2) + start2
+    """Maps value from an initial range in (start1, stop1) to a final range (start2, stop2)"""
+    return (value - start1) / (stop1 - start1) * (stop2 - start2) + start2
 
 
 class CMYK_Halftone(vsketch.SketchClass):
@@ -29,10 +35,9 @@ class CMYK_Halftone(vsketch.SketchClass):
     image = vsketch.Param(IMG_PATH, choices=images)
     orient = vsketch.Param("landscape", choices=["portrait", "landscape"])
     center = vsketch.Param(True)
-    pitch = vsketch.Param(0.5, 0.0, unit="cm", step=0.125, decimals=3)
     num_x = vsketch.Param(100, 1)
     scale = vsketch.Param(0.8)
-    use_k_channel = vsketch.Param(False) # not working with PIL
+    use_k_channel = vsketch.Param(False)  # not working with PIL
     pen_width = vsketch.Param(0.3, unit="mm", step=0.05)
     max_radius = vsketch.Param(0.7)
 
@@ -40,6 +45,7 @@ class CMYK_Halftone(vsketch.SketchClass):
         vsk.size("a4", landscape=(self.orient == "landscape"), center=self.center)
         vsk.penWidth(self.pen_width)
 
+        # Gets page size
         if self.orient == "landscape":
             page_h, page_w = vp.convert_page_size("a4")
         else:
@@ -48,64 +54,77 @@ class CMYK_Halftone(vsketch.SketchClass):
         img = Image.open(IMG_PATH).convert("RGBA")
         x_pixels, y_pixels = img.size
 
-        num_y = int(np.floor(self.num_x*y_pixels/x_pixels))
+        # Number of vertical steps
+        num_y = int(np.floor(self.num_x * y_pixels / x_pixels))
+        # Convert image to have (num_x, num_y) pixels
         img_transf = img.resize((self.num_x, num_y))
+        # Convert image to array to be able to apply rotation
         img_array = np.array(img_transf)
 
+        # Size for a square image that can fit any rotation of the input image
         max_size = int(np.ceil(np.sqrt(self.num_x**2 + num_y**2)))
 
-        final_x = np.ceil(self.scale*page_w*max_size/self.num_x)
-        max_radius = int(round(self.max_radius*final_x/(2*max_size)))
+        # Size of final image
+        final_x = np.ceil(self.scale * page_w * max_size / self.num_x)
 
-        print('Page size:', page_w, page_h)
-        print('Final image size:', final_x)
-        print('Pixels:', self.num_x, num_y)
-        print('Max circle radius:', max_radius)
+        # Max circle radius
+        max_radius = int(round(self.max_radius * final_x / (2 * max_size)))
 
+        print("Page size:", page_w, page_h)
+        print("Final image size:", final_x)
+        print("Pixels:", self.num_x, num_y)
+        print("Max circle radius:", max_radius)
+
+        # Append black if using K channel
+        # Option not working with PIL
         if self.use_k_channel:
-            CMYK_COLORS.append((0,0,0))
-        
-        print(CMYK_COLORS)
-        print(len(CMYK_ANGLES))
-        
+            CMYK_COLORS.append((0, 0, 0))
+
+        # Loop for each color channel
         for i, _ in enumerate(CMYK_COLORS):
             angle = CMYK_ANGLES[i]
-            
-            img_r = ndimage.rotate(img_array, angle)
 
+            # Set color layer
+            vsk.stroke(i + 1)
+            vsk.fill(i + 1)
+
+            # Rotate image
+            img_r = ndimage.rotate(img_array, angle)
+            # Convert array to PIL image
             img_transf = Image.fromarray(img_r)
 
+            # Paste rotated image in background image to ensure that all
+            # image iterations have the same squared size, are centered
+            # and have a white background
             img_w, img_h = img_transf.size
-            print(f'Rotated image size {i}:', img_w, img_h)
-
             offset = (round((max_size - img_w) / 2), round((max_size - img_h) / 2))
-            print('Offset:', offset)
 
-            img_bg = Image.new("RGBA", (max_size, max_size), color=(255,255,255)) # Create a white rgba background
-            img_bg.paste(img_transf, offset, img_transf)   
-            img_bg.convert('CMYK') 
+            img_bg = Image.new(
+                "RGBA", (max_size, max_size), color=(255, 255, 255)
+            )  # Create a white rgba background
+            img_bg.paste(img_transf, offset, img_transf)
+            img_bg.convert("CMYK")
 
-            print(img_bg.getpixel((0,0)))
+            print(f"Rotated image size {i}:", img_w, img_h)
+            print("Offset:", offset)
 
-            vsk.stroke(i+1)
-            vsk.fill(i+1)
-
+            # Loop through rotated image pixels
             for x in range(max_size):
-                pos_x = map_range(x, 0, max_size, -final_x/2, final_x/2)
+                pos_x = map_range(x, 0, max_size, -final_x / 2, final_x / 2)
                 for y in range(max_size):
-                    pos_y = map_range(y, 0, max_size, -final_x/2, final_x/2)
-                    p = img_bg.getpixel((x,y))[i]
+                    pos_y = map_range(y, 0, max_size, -final_x / 2, final_x / 2)
+
+                    # Get color intensity
+                    p = img_bg.getpixel((x, y))[i]
+                    # Circle size according to intensity
                     circle_size = cmyk_radius(p, max_radius)
 
+                    # Rotate image and add circle to sketch
                     with vsk.pushMatrix():
                         vsk.rotate(angle, degrees=True)
-                        #if i == 3:
-                        #    print(circle_size)
+                        vsk.circle(pos_x, pos_y, radius=circle_size, mode="center")
 
-                        vsk.circle(pos_x,pos_y,radius=circle_size, mode='center')
-    
-
-    
+        # Define CMYK layer colors
         vsk.vpype("color --layer 1  #00FFFF")
         vsk.vpype("color --layer 2  #FF00FF")
         vsk.vpype("color --layer 3  #FFFF00")
@@ -116,4 +135,4 @@ class CMYK_Halftone(vsketch.SketchClass):
 
 
 if __name__ == "__main__":
-     CMYK_Halftone.display()
+    CMYK_Halftone.display()
